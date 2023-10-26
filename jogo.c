f8b7cda78852146906481f3fa68cd603f1aa9f5d
Gabriel Lopes <gabriellopes1812@outlook.com>
2023-10-25 21:35:58 -0300

    error linha 31

--- jogo.c
+++ jogo.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 #include <string.h>
+char* gerarPalavraAleatoria() {
+	char* palavras[] = {
+		"banana",
+		"computador",
+		"teclado",
+		"janela",
+		"programacao"
+	};
 
+	int numeroDePalavras = sizeof(palavras) / sizeof(palavras[0]);
+
+	srand(time(NULL));
+
+	int indiceAleatorio = rand() % numeroDePalavras;
+return palavras[indiceAleatorio];
+}
 void escolherPalavra(char with[]) {
 
 	printf("Escolha uma palavra!\n");
@@ -11,17 +26,26 @@ int main() {
 	char palavra[40];
 	int maxErros = 6;
 	int errosAtuais = 0;
+	char result[40] ;
+
+	gerarPalavraAleatoria() = result;
 
 	escolherPalavra(palavra);
 
 	while (errosAtuais < maxErros)
 	{
-		printf("continue tentando!");
+		if (palavra != result){
+			printf("continue tentando!");
+			errosAtuais++;
+			escolherPalavra(palavra);
+		}
+			if (errosAtuais==5){
+			printf("Sua ultima chance!");
+			}
+		else if (palavra == result){
+			printf("A palavra escolhida foi: %s\n", palavra);
+		}
 	}
-
-	printf("A palavra escolhida foi: %s\n", palavra);
-
-	// Restante do código...
 
 	return 0;
 }
